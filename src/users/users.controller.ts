2edab98b930cb8614cf5d30f73682b7ecdb6a83c
sujitha <sujitha.agamworks@gmail.com>
2024-12-03 07:19:07 +0530

    learned GET,POST,PATCH,DELETE

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,46 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { query } from 'express';
 import { get } from 'http';
-
+import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
 
+constructor(private readonly userService:UsersService)
+{
 
+}
     @Get()
-    findAllUsers()
+    findAllUsers(@Query('role') role?: "ADMIN"|"INTERN")
     {
-        return "find all users";
+      return this.userService.findAll(role);
     }
-    @Get('interns')
-    findAllinterns()
-    {
-        return [];
-    }
+    @Get(':id')
+   findOneUser(@Param('id') id:number)
+   {
+return this.userService.findOne(id);
+   }
     @Get(':id')
     findSingleUser(@Param('id') id:string)
     {
         return {id};
     }
 
- 
+ @Post()
+ createUser(@Body() user:{})
+ {
+return user;
+ }
+
+ @Patch(":id")
+ updateUser(@Param("id") id:string,@Body() userData:{})
+ {
+    return {id,...userData};
+ }
+
+ @Delete(":id")
+deleteTheUser(@Param("id") myParam:string)
+{
+    return {myParam};
+}
+
 }
